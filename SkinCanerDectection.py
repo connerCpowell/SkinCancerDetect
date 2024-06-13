424f12b514b964418e0ed62b7fe74a0f65995755
connerCpowell <copowengineering@gmail.com>
2024-06-13 07:33:12 -0700

    Adding first graphs

--- SkinCanerDectection.py
+++ SkinCanerDectection.py
@@ -53,3 +53,33 @@ df.head()
 
 
 # %%
+
+x = df['label'].value_counts()
+plt.pie(x.values,
+        labels=x.index,
+        autopct='%1.1f%%')
+plt.show()
+
+
+# %%
+
+for cat in df['label'].unique():
+    temp = df[df['label'] == cat]
+ 
+    index_list = temp.index
+    fig, ax = plt.subplots(1, 4, figsize=(15, 5))
+    fig.suptitle(f'Images for {cat} category . . . .', fontsize=20)
+    for i in range(4):
+        index = np.random.randint(0, len(index_list))
+        index = index_list[index]
+        data = df.iloc[index]
+ 
+        image_path = data[0]
+ 
+        img = np.array(Image.open(image_path))
+        ax[i].imshow(img)
+plt.tight_layout()
+plt.show()
+
+
+# %%
